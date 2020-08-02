9d26ff93102416c8df9ef055040ab6f767dc30d4
kiranashraf <51193308+kiranashraf@users.noreply.github.com>
2020-08-02 20:37:59 +0200

    Delete hello_lambda.py

--- hello_lambda.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def is_anagram(input1, input2):
-    """Check if two functions are anagrams of each other."""
-    return sorted(input1.upper()) == sorted(input2.upper())
-
-def lambda_handler(event, context):
-    return is_anagram(event['input1'], event['input2'])
